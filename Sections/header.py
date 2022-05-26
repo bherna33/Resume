42799983d7e4b7a48a3dccf35b2187f85ef47bc8
Branden Henrnadez <Brandenjh98@gmail.com>
2022-05-26 12:09:42 -0400

    first commit

---
+++ Sections/header.py
@@ -0,0 +1,6 @@
+from pylatex import Document, PageStyle, Head, MiniPage, Foot, LargeText, \
+    MediumText, LineBreak, simple_page_number
+from pylatex.utils import bold
+
+def header():
+    print("test")
