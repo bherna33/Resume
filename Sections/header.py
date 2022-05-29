0a90b3628d3db09ddef0cd478eede3dd4e1f1d6b
Branden Henrnadez <Brandenjh98@gmail.com>
2022-05-29 16:21:46 -0400

    rre organized code and added a template folder

--- Sections/header.py
+++
@@ -1,6 +0,0 @@
-from pylatex import Document, PageStyle, Head, MiniPage, Foot, LargeText, \
-    MediumText, LineBreak, simple_page_number
-from pylatex.utils import bold
-
-def header():
-    print("test")
