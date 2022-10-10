673785fb7d6cf1721e7cff0e699aad13088bf690
Branden Henrnadez <Brandenjh98@gmail.com>
2022-10-10 15:46:32 -0400

    Changed Main file and added Template 2

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from Template import template1
 from pylatex import Document
 import json
 
+from user import User
+
 #read in json file of user data
 with open('Userdata.json', 'r') as jsonFile:
         userdata = json.load(jsonFile)
         
-doc = Document(documentclass='article', document_options='11pt',geometry_options={"margin":"0.5in"})
 
 # contents for contant info
 name = userdata[0]
@@ -43,7 +44,10 @@ other = userdata[12]
 
 
 #template 1 
-def temp1():
+def temp1(file_name):
+    doc = Document(documentclass='article', document_options='11pt',geometry_options={"margin":"0.5in"})
+    template1.packages(doc)
+    template1.commands(doc)
     template1.header(doc,name, loc, email, phone, linkedIn, otherContacts)
     template1.summary(doc, summary)
     template1.education(doc, education)
@@ -51,9 +55,26 @@ def temp1():
     template1.keySkills(doc,skills)
     template1.certification(doc, cert)
     template1.other(doc, otherName, other)
-    doc.generate_tex('TestFile')
+    doc.generate_tex(file_name)
+    
+    #deletes test file
+
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+
+def temp2():
+    print("hi")
 
 
 if __name__ == '__main__':
-  
-    temp1()
+    User
+    resumenum = input("What resume would you pick?\n")
+    if resumenum == "1":
+        file_name = input("What would you like to name your resume?\n")
+        temp1(file_name)
+    if resumenum == "2":
+        temp2()
+    else:
+        print("I have not been created yet")
+    
--- user.py
+++ user.py
@@ -1,71 +1,72 @@
 import json
 
-# contents for contant info
-name = ""
-loc = ""
-email = ""
-phone = ""
-linkedIn = ""
-otherContacts = []
+class User():
+    def __init__(self):
+        # contents for contant info
+        self.name = input("what is your name?")
+        self.loc = "Miami, Fl"
+        self.email = "Testemail@email.com"
+        self.phone = "1234567890"
+        self.linkedIn = "bhernan637389"
+        self.otherContacts = []
 
-# contents for summary
-summary = ""
+        # contents for summary
+        self.summary = "this is a test to see if everything i great and dandy. once it is i can \
+            continue with tis and make more progresss to ths document\
+            i really dont know how to move forward with this, i might  ake a window to add the info to make\
+            things erier. we will see."
 
-# contents for education
-#hashmap{String: hashmap{String: hashmap{String: String}}}
-education = {
-    "University Near You":
-    {"city, state":
-        {"Bachlor of Science in Computer science":
-            "1/2022"}
-        },
-    "University Near you":
-        {"city, state":
-            {"Bachlors of buisness":
-                "2/2020"}
-            }
-        }
+        # contents for education
+        #hashmap{String: hashmap{String: hashmap{String: String}}}
+        self.school = {}
 
-# contents for job
-#hashmap{String : hashmap{String: hashmap{String: hashmap{String:Array[Strings]}}}}
-#hashmap{"job" : hashmap{"city, state": hashmap{"job name": hashmap{"date":Array[responsibilities]}}}}
-exp = {
-    "College of engerning" : 
-    {"city, state": 
-        {"Technical Assistant": 
-            {"july 2016- current":
-                ["Assistant director", "technical supprot", "mannanged and directed"]
-                }
-            }
-        },
-    "College of buisnness":
-        {"city, state":
-            {"inter assistant":
-                {"jan 2016- july 2016":
-                    ["built computers", "managed money", "file stuff"]
-                    }
-                }
-            }
-    }
+        # contents for job
+        #hashmap{String : hashmap{String: hashmap{String: hashmap{String:Array[Strings]}}}}
+        #hashmap{"job" : hashmap{"city, state": hashmap{"job name": hashmap{"date":Array[responsibilities]}}}}
+        self.exp = {}
 
-# contents for key skills
-skills = ""
+        # contents for key skills
+        self.skills = "key skill1, keyskill2, keyskills3, 4,5,6,7,8,9"
 
-#contents for certifications
-#hashmap{String: String}
-#certifiction : date
-cert = {}
+        #contents for certifications
+        #hashmap{String: String}
+        #certifiction : date
+        self.cert = {}
 
-#contents for other
-#hashmap{String: String}
-#hasmap{other: date}
-otherName = ""
-other = {}
+        #contents for other
+        #hashmap{String: String}
+        #hasmap{other: date}
+        self.misName = "Other Stuff"
+        self.other = {}
 
-#TODO: Add way for user to put file name
-#filename = ""
+        #TODO: Add way for user to put file name
+        #self.filename = ""
+        
+        
+    def education(self, college, state, degree, date):
+        self.school = {college:{state:{degree:date}}}
+        return self.school
+            
+    def experence(self, job,location, title, date, responsiblities):
+        self.exp = {job: {location: {title: {date:responsiblities}}}}
+        return self.exp
+    
+    def certifications(self, name, date):
+        self.cert = {name:date}
+        return self.cert
+    
+    def misc(self, name, date):
+        self.other = {name:date}
+        return self.other
+            
+        
+user = User()
+education = user.education("college", "state", "degree", "month/year")
+experence = user.experence("Job1", "city/state", "title1", "month/year", ["duty1", "duty2", "duty3"])
+certification = user.certifications("certification 1", "month/year")
+misc = user.misc("name", "month/year")
 
 
 with open("Userdata.json", "w") as jsonfile:
 
-    json.dump((name, loc,email,phone, linkedIn, otherContacts, summary, education, exp,skills, cert,otherName,other), jsonfile, indent=4)
+    json.dump((user.name, user.loc,user.email,user.phone, user.linkedIn, user.otherContacts, user.summary, education, experence,user.skills, certification,user.misName,misc), jsonfile, indent=4)
